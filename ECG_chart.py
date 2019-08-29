b463dc0022dfb3755367a83542fe9190b065e5fd
Maksim Krivolapov <givago@mail.ru>
2019-08-29 13:14:49 +0200

    dark background style

--- ECG_chart.py
+++ ECG_chart.py
@@ -9,3 +9,28 @@ for i in range(0, 5):
     plt.axvspan(i, i+.5, facecolor='b', alpha=0.5)
 
 plt.show()
+
+
+############## DARK BACKGROUND
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.style.use('dark_background')
+
+fig, ax = plt.subplots()
+
+L = 6
+x = np.linspace(0, L)
+ncolors = len(plt.rcParams['axes.prop_cycle'])
+shift = np.linspace(0, L, ncolors, endpoint=False)
+for s in shift:
+    ax.plot(x, np.sin(x + s), 'o-')
+ax.set_xlabel('x-axis')
+ax.set_ylabel('y-axis')
+ax.set_title("'dark_background' style sheet")
+
+plt.show()
+
+################
